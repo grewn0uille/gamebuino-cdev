1cfb9253fdf915d6dfcac1528e6843ca2b46acd1
Lucie Anglade <lucie@courtbouillon.org>
2021-06-13 14:41:35 +0200

    Base structure

---
+++ base-structure.ino
@@ -0,0 +1,16 @@
+#include <Gamebuino-Meta.h>
+
+void setup() {
+  gb.begin();
+}
+
+void loop() {
+  while(!gb.update());
+  gb.display.clear();
+
+  gb.display.setColor(RED, YELLOW);
+
+  gb.display.setFontSize(2);
+
+  gb.display.print("hello, world\nbonjour, monde\nhola, mundo");
+}
